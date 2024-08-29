31d322d00a6a0e1a2f683993a1fc0d4684ed9b9e
MadalenaMartins <madalena_parreira_martins@hotmail.com>
2024-08-29 19:36:08 +0100

    Category

---
+++ src/main/java/com/madalenajava/course/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.madalenajava.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.madalenajava.course.entities.Category;
+import com.madalenajava.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findall() {
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
